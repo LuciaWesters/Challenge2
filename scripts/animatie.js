7c33e88046cf006b0731344f9c76a96085ad473c
LuciaWesters <79092031+LuciaWesters@users.noreply.github.com>
2021-03-12 13:17:19 +0100

    fasenDag

--- scripts/animatie.js
+++ scripts/animatie.js
@@ -6,9 +6,15 @@ var achtergrond = document.getElementById('achtergrond');
 
 var today = new Date().getHours();
 
-if (today >= 7 && today <= 22) {
+if (today >= 7 && today <= 10) {
+  moonImage.classList.add('hidden');
+  achtergrond.classList.add('ochtend');
+} else if (today >= 11 && today <= 18) {
   moonImage.classList.add('hidden');
   achtergrond.classList.add('licht');
-} else {
+} else if (today >= 19 && today <= 21) {
+  moonImage.classList.add('hidden');
+  achtergrond.classList.add('avond');
+} else{
   sunImage.classList.add('hidden');
 }
